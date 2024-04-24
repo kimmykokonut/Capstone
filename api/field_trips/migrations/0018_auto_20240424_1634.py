374b423f27efb3547caf512f05ad983444aaa9ab
Kim Robinson <kimmykokonut@gmail.com>
2024-04-24 09:51:20 -0700

    seed permit model in database

--- api/field_trips/migrations/0018_auto_20240424_1634.py
+++ api/field_trips/migrations/0018_auto_20240424_1634.py
@@ -2,6 +2,60 @@
 
 from django.db import migrations
 
+def create_permits(apps, schema_editor):
+    Permit = apps.get_model('field_trips', 'Permit')
+    permits_data = [
+        {'name': 'Discover Pass',
+         'type': 'State',
+         'day_cost': 11.50,
+         'annual_cost': 35.00},
+         {'name': 'Oregon State Parks',
+         'type': 'State',
+         'day_cost': 5.00,
+         'annual_cost': 30.00},
+         {'name': 'Northwest Forest Pass',
+         'type': 'Federal',
+         'day_cost': 5.00,
+         'annual_cost': 30.00},
+         {'name': 'America the Beautiful',
+         'type': 'Federal',
+         'day_cost': 0,
+         'annual_cost': 80.00},
+         {'name': 'Oregon Fish & Wildlife-parking',
+         'type': 'State',
+         'day_cost': 10,
+         'annual_cost': 30},
+         {'name': 'Oregon Fish & Wildlife-forage Sauvie Island',
+         'type': 'State',
+         'day_cost': 0,
+         'annual_cost': 0},
+         {'name': 'Mt Hood National Forest',
+         'type': 'Federal',
+         'day_cost': 0,
+         'annual_cost': 0},
+         {'name': 'Gifford Pinchot National Forest',
+         'type': 'Federal',
+         'day_cost': 0,
+         'annual_cost': 0},
+         {'name': 'Ochoco National Forest',
+         'type': 'Federal',
+         'day_cost': 0,
+         'annual_cost': 0},
+         {'name': 'Deschutes National Forest',
+         'type': 'Federal',
+         'day_cost': 0,
+         'annual_cost': 0},
+         {'name': 'Fremont-Winema National Forest',
+         'type': 'Federal',
+         'day_cost': 0,
+         'annual_cost': 0},
+         {'name': 'Umpqua National Forest',
+         'type': 'Federal',
+         'day_cost': 0,
+         'annual_cost': 0},
+    ]
+    for permit_data in permits_data:
+        Permit.objects.create(**permit_data)
 
 class Migration(migrations.Migration):
 
@@ -10,4 +64,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(create_permits)
     ]
