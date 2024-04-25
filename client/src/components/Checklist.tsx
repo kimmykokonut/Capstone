19a0eadb012f1f18ddc0151ad477f94cb15d20b4
Kim Robinson <kimmykokonut@gmail.com>
2024-04-25 16:04:20 -0700

    checklist complete with print button

--- client/src/components/Checklist.tsx
+++ client/src/components/Checklist.tsx
@@ -1,7 +1,11 @@
 const Checklist = () => {
-  
+  const handlePrint = () => {
+    window.print();
+  };
+
   return (
     <>
+    <button onClick={handlePrint}>Print Me</button>
       <h2>Mushroom Hunting Checklist</h2>
       <p>Mushroom hunting is generally a fun day trip!  But we all know that it is not all that difficult to get turned around and lost.  PLEASE be prepared with the following items, and ask yourself: Could I survive a night in the woods with the gear that I am carrying?</p>
       <h3>Essential Items</h3>
@@ -30,33 +34,33 @@ const Checklist = () => {
         <li>Rain jacket with hood</li>
         <li>Rain pants</li>
       </ul>
-      
-      <ul>
-      <li>Sturdy, moisture-proof boots (no street shoes, tennis shoes or sandals)</li>
-        <li>Hat(s) <em>for warmth/shade from sun/repel rain</em></li>
-          <li>Mittens or gloves </li>
-          <li>Brightly colored / Highly visible / Reflective vest or jacket</li>
-        </ul>
- 
-        <h3>In your flat-bottomed basket:</h3>
-        <ul>
-          <li>Knife, brightly marked</li>
-          <li>Brush for cleaning mushrooms prior to putting them in your basket</li>
-          <li>Waxed or paper bags</li>
-        </ul>
 
-        <h3>Extras</h3>
-        <ul>
-          <li>Lunch</li>
-          <li>Trowel and toilet paper, and a bag to pack out your t.p.</li>
-          <li>Insect repellent</li>
-          <li>Field guides and key</li>
-          <li>Camera</li>
-          <li>Flagging tape</li>
-          <li>Two or more FRS walkie-talkies. This type of radio is small, effective over several miles and often modestly priced. FRS radios do not require a license.</li>
-        </ul> 
+      <ul>
+        <li>Sturdy, moisture-proof boots (no street shoes, tennis shoes or sandals)</li>
+        <li>Hat(s) <em>for warmth/shade from sun/repel rain</em></li>
+        <li>Mittens or gloves </li>
+        <li>Brightly colored / Highly visible / Reflective vest or jacket</li>
+      </ul>
+
+      <h3>In your flat-bottomed basket:</h3>
+      <ul>
+        <li>Knife, brightly marked</li>
+        <li>Brush for cleaning mushrooms prior to putting them in your basket</li>
+        <li>Waxed or paper bags</li>
+      </ul>
+
+      <h3>Extras</h3>
+      <ul>
+        <li>Lunch</li>
+        <li>Trowel and toilet paper, and a bag to pack out your t.p.</li>
+        <li>Insect repellent</li>
+        <li>Field guides and key</li>
+        <li>Camera</li>
+        <li>Flagging tape</li>
+        <li>Two or more FRS walkie-talkies. This type of radio is small, effective over several miles and often modestly priced. FRS radios do not require a license.</li>
+      </ul>
     </>
-      )
+  )
 }
 
-      export default Checklist; 
+export default Checklist; 
