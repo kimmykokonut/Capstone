bda61a535f0adda2d958e7086b712ebca49a0a70
Kim Robinson <kimmykokonut@gmail.com>
2024-04-30 15:53:36 -0700

    style triplist component

--- client/src/components/Trip.tsx
+++ client/src/components/Trip.tsx
@@ -17,16 +17,16 @@ const Trip: React.FC<TripProps> = ({ id, date, general_location, restrictions, i
 
   return (
     <>
-      <Card variant="outlined" sx={{ maxWidth: 350, m: '1rem', height: 'auto', backgroundColor: '#e0e0e0' }}>
+      <Card
+        variant="outlined"
+        sx={{ maxWidth: 350, m: '1rem', height: '100%', display: 'flex', flexDirection: 'column', backgroundColor: '#e0e0e0' }}>
         <CardMedia
           sx={{ height: 150 }}
           image={image_url}
           title="mushrooms in the forest"
         />
         <CardContent
-          style={{ marginTop: 'auto' }}
-          sx={{ height: 150, display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center' }}>
-
+          sx={{ mt: 1, height: 150, flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center' }}>
           <Typography gutterBottom variant="h5" component="div">
             {dateString}
           </Typography>
@@ -39,19 +39,19 @@ const Trip: React.FC<TripProps> = ({ id, date, general_location, restrictions, i
             Restrictions: {restrictions || 'None'}          </Typography>
         </CardContent>
         <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <CardActions>
-          <Link to={`/trips/${id}`}>
-            <Button 
-            variant="outlined" 
-            color="success" 
-              sx={{ 
-    borderColor: 'green',
-    color: 'green',
-              '&:hover': {backgroundColor: 'green', color: 'white' },
-              '&:focus': {outline: '2px solid red' }
-  }}>Details/Register</Button>
-          </Link>
-        </CardActions>
+          <CardActions>
+            <Link to={`/trips/${id}`}>
+              <Button
+                variant="outlined"
+                color="success"
+                sx={{
+                  borderColor: 'green',
+                  color: 'green',
+                  '&:hover': { backgroundColor: 'green', color: 'white' },
+                  '&:focus': { outline: '2px solid red' }
+                }}>Details/Register</Button>
+            </Link>
+          </CardActions>
         </Box>
       </Card>
     </>
