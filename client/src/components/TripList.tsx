6a1dffa7d850d6f0a642b6f683a00f8fb793d729
Kim Robinson <kimmykokonut@gmail.com>
2024-05-25 14:48:10 -0700

    update styling triplist and trip

--- client/src/components/TripList.tsx
+++ client/src/components/TripList.tsx
@@ -1,6 +1,6 @@
 import Trip from "./Trip";
 import { Link } from "react-router-dom";
-import { Grid, Typography, Button } from "@mui/material";
+import { Grid, Typography, Button, Box } from "@mui/material";
 
 interface TripListProps {
   trips: {
@@ -15,17 +15,17 @@ interface TripListProps {
   }[];
 }
 interface TripProps {
-    id: number;
-    date: string;
-    general_location: string;
-    restrictions: string;
-    image_url: string;
-    status: string;
-  };
+  id: number;
+  date: string;
+  general_location: string;
+  restrictions: string;
+  image_url: string;
+  status: string;
+};
 
 const TripList: React.FC<TripListProps> = ({ trips }) => {
   const groupStatus = JSON.parse(localStorage.getItem('group_status') || '[]');
-  
+
   if (!trips) {
     return <div>Loading...</div>
   }
@@ -33,35 +33,38 @@ const TripList: React.FC<TripListProps> = ({ trips }) => {
     <>
       <Typography variant="h4" align="center" mt="30px">Upcoming Field Trips</Typography>
       <hr />
-      { (groupStatus.includes('Leader') || groupStatus.includes('Coordinator')) && (
-        <Button
-          variant="outlined"
-          color="success"
-          sx={{
-            borderColor: 'green',
-            color: 'green',
-            '&:hover': { backgroundColor: 'green', color: 'white' },
-            '&:focus': { outline: '2px solid red' }
-          }}>
-          <Link to='/add-trip' style={{ textDecoration: 'none', color: 'inherit' }}>Add new trip</Link>
-        </Button>
+      {(groupStatus.includes('Leader') || groupStatus.includes('Coordinator')) && (
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Button
+            variant="outlined"
+            color="success"
+            sx={{
+              borderColor: 'green',
+              color: 'green',
+              '&:hover': { backgroundColor: 'green', color: 'white' },
+              '&:focus': { outline: '2px solid red' }
+            }}>
+            <Link to='/add-trip' style={{ textDecoration: 'none', color: 'inherit' }}>Add new trip</Link>
+          </Button>
+        </Box>
       )}
-      
+
       <Grid container spacing={2}>
-          {trips.map((trip: TripProps) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={trip.id}>
-
-        <Trip
-          id={trip.id}
-          date={trip.date}
-          general_location={trip.general_location}
-          restrictions={trip.restrictions}
-          image_url={trip.image_url}
-          status={trip.status}
-        />
-              </Grid>
-
-      ))}
+        {trips.map((trip: TripProps) => (
+          <Grid 
+            item xs={12} sm={6} md={4} lg={3} 
+            key={trip.id}
+            >
+            <Trip
+              id={trip.id}
+              date={trip.date}
+              general_location={trip.general_location}
+              restrictions={trip.restrictions}
+              image_url={trip.image_url}
+              status={trip.status}
+            />
+          </Grid>
+        ))}
       </Grid>
     </>
   );
